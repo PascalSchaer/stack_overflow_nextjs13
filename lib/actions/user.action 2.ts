84da7acceeafd1be1586310d0a4f0f156b5c353e
Pascal <pascalschaer8@gmail.com>
2023-11-30 11:21:55 -0700

    Fix sheet.tsx for deployment

---
+++ lib/actions/user.action 2.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { connectToDatabase } from "../mongoose";
+import User from "@/database/user.model";
+import { CreateUserParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+
+export async function getUserById(params: any) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+
+    const user = await User.findOne({ clerkId: userId });
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const newUser = await User.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    await User.findOneAndUpdate({ clerkId }, updateData, { new: true });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
